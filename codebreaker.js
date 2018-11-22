d77543080ad8b836a6e98aab1869496b5ab26c94
LuisaVictoria18 <lfvf00@gmail.com>
2018-11-21 23:05:32 -0500

    juego con testing

--- codebreaker.js
+++ codebreaker.js
@@ -1,9 +1,40 @@
-var realNumber = "1234";
+var realNumber = "5612";
 
- const guess = function(number) {
+const guess = function(number1) {
   // TODO: Logic Here
+  let result = "";
+  let real = realNumber.split("");
+  let actual = number1.split("");
+  var yes = false;
 
-  return "XXXX";
+  for (let i=0; i<actual.length; i++){
+
+    yes = false;
+
+    for (let y=0; y<real.length; y++){
+      if(real[i] === actual[y]){
+
+        yes = true; 
+
+        if(i == y){
+          result = result + "x";
+        }else{
+        result = result + "o";
+        }
+      }
+    }
+   
+  }
+ 
+    return result;
+
+
 }
 
-module.exports = guess;
+const setNumber = function (number1) {
+  realNumber = number1;
+}
+
+
+module.exports.guess = guess;
+module.exports.setNumber = setNumber;
--- test/codebreaker.test.js
+++ test/codebreaker.test.js
@@ -9,7 +9,74 @@ describe( 'Test pass', function(){
 })
 
 describe('Codebreaker Test', function(){
-    it('This is a test case', function(){
-        // TODO: Test case here
+        beforeEach(function () {
+            guess.setNumber('5612')
     })
+
+    it('Debe retornar un ox', function(){
+        let result= guess.guess("0517");
+
+        test.equal(result,"ox","Posición equivocada");
+    })
+
+    it('Debe retornar un oo', function(){
+        let result=guess.guess("1345");
+
+        test.equal(result,"oo","Posición equivocada");
+    })
+
+    it('Debe retornar un " " ', function(){
+        let result=guess.guess("4899");
+
+        test.equal(result,"","Posición equivocada");
+    })
+
+    it('Debe retornar un oo', function(){
+        let result=guess.guess("7321");
+
+        test.equal(result,"oo","Posición equivocada");
+    })
+
+    it('Debe retornar un o', function(){
+        let result=guess.guess("1874");
+
+        test.equal(result,"o","Posición equivocada");
+    })
+
+    it('Debe retornar un xx', function(){
+        let result=guess.guess("9632");
+
+        test.equal(result,"xx","Posición equivocada");
+    })
+
+    it('Debe retornar un oo', function(){
+        let result=guess.guess("3056");
+
+        test.equal(result,"oo","Posición equivocada");
+    })
+
+    it('Debe retornar un ox', function(){
+        let result=guess.guess("1842");
+
+        test.equal(result,"ox","Posición equivocada");
+    })
+
+    it('Debe retornar un xxx', function(){
+        let result=guess.guess("5613");
+
+        test.equal(result,"xxx","Posición equivocada");
+    })
+
+    it('Debe retornar un xxxx', function(){
+        let result=guess.guess("5612");
+
+        test.equal(result,"xxxx","Posición correcta");
+    })
+   
+
+
+
+
+
+
 })
